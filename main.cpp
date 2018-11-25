3d6419e78c438a4a09c8f674fadfdb31c8fa445c
Max <maxi4o@mail.ru>
2018-11-26 00:32:41 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -21,7 +21,12 @@ void simple_sort(int *A, int N)
             A[j] = t;
         }
 }
-
+void print(int *arr,int n){
+    std::cout << '\n';
+    for( int i = 0; i < n; i++ ) {
+        std::cout << arr[i] << ' ';
+    }
+}
 struct swap_struct {
     int *arr;
     int n;
@@ -30,21 +35,24 @@ struct swap_struct {
     int *k;
 };
 void *thread(void *args){
-    swap_struct *arguments = (swap_struct*) args;
-    int t;
-    int *arr = arguments->arr;
-    int *i = arguments->i;
-    int *j = arguments->j;
-    int *k = arguments->k;
-    t = arr[i[0]];
-    for( j[0] = i[0]; j[0] >= k[0]; j[0] -= k[0] )
-    {
+    /*swap_struct *arguments = (swap_struct*) args;
+       int t;
+       int *arr = arguments->arr;
+       int *i = arguments->i;
+       int *j = arguments->j;
+       int *k = arguments->k;
+       t = arr[i[0]];
+       for( j[0] = i[0]; j[0] >= k[0]; j[0] -= k[0] )
+       {
         if( t < arr[j[0] - k[0]] )
             arr[j[0]] = arr[j[0] - k[0]];
         else
             break;
-    }
-    arr[j[0]] = t;
+       }
+       arr[j[0]] = t;
+     */
+    int *e = (int*)args;
+    std::cout << e[0] << '\n';
     pthread_exit(0);
 }
 void func(int *arr,int n){
@@ -52,22 +60,53 @@ void func(int *arr,int n){
     int i,j,k;
     int t;
     for( k = n / 2; k > 0; k /= 2 ) {
-        pthread_t *th = new pthread_t[n - k];
-        for( i = k; i < n; i++ )
-        {
-            pthread_t th;
-            swap_struct args;
-            args.arr = arr;
-            args.n = n;
-            args.i = &i;
-            args.j = &j;
-            args.k = &k;
-            pthread_create(&th,NULL,thread,(void*)&args);
+        int num = n - k + 1;
+        pthread_t *th = new pthread_t[n - k + 1];
+        int l = 0;
+        for( i = k; i < n; i++ ) {
+            pthread_create(&( th[l] ),NULL,thread,(void*)&l);
+            l++;
         }
-
+        for ( int p = 0; p < l; p++ ) {
+            pthread_join(th[p],NULL);
+        }
+        std::cout << "/* message */" << '\n';
+        /*pthread_t *th = new pthread_t[n - k + 1];
+           for( i = k; i < n; i++ )
+           {
+            swap_struct *args = new swap_struct;
+            args->arr = arr;
+            args->n = n;
+            args->i = &i;
+            args->j = &j;
+            args->k = &k;
+            int e = pthread_create(&( th[i - k] ),NULL,thread,(void*)args);
+            std::cout << i << '\n';
+           }
+           for( int l = 0; l < n - k; l++ ) {
+            std::cout << pthread_join(th[i],NULL) << '\n';
+           }
+           delete th;*/
     }
 }
 
+void *some_func(void* args){
+    int *arr = (int*)args;
+    for( int i = 0; i < 10000; i++ ) {
+        std::cout << arr[i] << '\n';
+    }
+}
+void foo(){
+    int n = 10000;
+    int *arr = new int[n];
+    for( int i = 0; i < n; i++ ) {
+        arr[i] = rand() % 100000 - 500;
+    }
+    pthread_t *pt = new pthread_t;
+    pthread_create(pt,NULL,some_func,(void*)arr);
+    pthread_join(pt[0],NULL);
+    std::cout << "/* message */" << '\n';
+}
 class ShellsSort {
 private:
     int *arr;
@@ -87,8 +126,9 @@ public:
 
 
 int main() {
+
     srand (time(NULL) );
-    int n = 10000000;
+    int n = 1000;
     clock_t t1, t2, t3, t4;
     int *arr = new int[n];
     int *arr2 = new int[n];
@@ -97,7 +137,8 @@ int main() {
     }
     memcpy(arr2,arr,sizeof( int ) * n);
     ShellsSort sh(arr,n);
-    ShellsSort sh2(arr,n);
+    ShellsSort sh2(arr2,n);
+
     /*for( int i = 0; i < n; i++ ) {
         std::cout << arr[i] << ' ';
        }
@@ -116,6 +157,6 @@ int main() {
         std::cout << arr2[i] << ' ';
        }*/
 
-    std::cout << "\nTIME without multithreading : " << ( t3 - t2 ) / (double)CLOCKS_PER_SEC;
+    /*std::cout << "\nTIME without multithreading : " << ( t3 - t2 ) / (double)CLOCKS_PER_SEC;*/
     return 0;
 }
