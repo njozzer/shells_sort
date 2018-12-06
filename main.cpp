08721470d5eb8f4197c8042f81bfe11d3b613c98
Max <maxi4o@mail.ru>
2018-12-06 13:30:40 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,8 +2,16 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <time.h>
-
-void simple_sort(int *A, int N)
+#include <cstring>
+#include <vector>
+void print(int *arr,int n){
+    std::cout << '\n';
+    for( int i = 0; i < n; i++ ) {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout <<  '\n';
+}
+void shell(int *A, int N)
 {
     unsigned i,j,k;
     int t;
@@ -21,73 +29,111 @@ void simple_sort(int *A, int N)
             A[j] = t;
         }
 }
-void print(int *arr,int n){
-    std::cout << '\n';
-    for( int i = 0; i < n; i++ ) {
-        std::cout << arr[i] << ' ';
-    }
+void simple_sort(int *arr, int n)
+{
+    unsigned i,j,k;
+    int t;
+    for( k = n / 2; k > 0; k /= 2 )
+        for( i = k; i < n; i++ )
+        {
+            //print(arr,n);
+            t = arr[i];
+            for( j = i; j >= k; j -= k )
+            {
+                if( t < arr[j - k] )
+                    arr[j] = arr[j - k];
+                else
+                    break;
+            }
+            arr[j] = t;
+        }
 }
-struct swap_struct {
+
+void merge_sort(int* arr,int n){
+    int i, key, j;
+    for ( i = 1; i < n; i++ )
+    {
+        key = arr[i];
+        j = i - 1;
+        while ( j >= 0 && arr[j] > key )
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+
+}
+
+struct B {
     int *arr;
+    int i;
     int n;
-    int *i;
-    int *j;
-    int *k;
+    int k;
+    int l;
 };
 void *thread(void *args){
-    /*swap_struct *arguments = (swap_struct*) args;
-       int t;
-       int *arr = arguments->arr;
-       int *i = arguments->i;
-       int *j = arguments->j;
-       int *k = arguments->k;
-       t = arr[i[0]];
-       for( j[0] = i[0]; j[0] >= k[0]; j[0] -= k[0] )
-       {
-        if( t < arr[j[0] - k[0]] )
-            arr[j[0]] = arr[j[0] - k[0]];
-        else
-            break;
-       }
-       arr[j[0]] = t;
-     */
-    int *e = (int*)args;
-    std::cout << e[0] << '\n';
-    pthread_exit(0);
+
+    B *b = (B*)args;
+
+    for( int i = 0; i < b->l; i++ ) {
+        arr[i] = &b->arr[b->arr2[i]];
+    }
+    for( int j = 0; j < n / k; j++ ) {
+        arr2[j] = i + j * k;
+        b->arr = arr;
+        b->arr2 = arr2;
+        b->n = n;
+        b->l = l;
+        //std::cout << arr2[j] << " ";
+    }
+    //i + j * k
+    int i, key, j;
+    for ( i = 1; i < n / k; i++ )
+    {
+        key = arr[i][0];
+        j = i - 1;
+        while ( j >= 0 && arr[j][0] > key )
+        {
+            arr[j + 1][0] = arr[j][0];
+            j = j - 1;
+        }
+        arr[j + 1][0] = key;
+    }
+    
+    delete b;
+
 }
+
 void func(int *arr,int n){
 
-    int i,j,k;
-    int t;
-    for( k = n / 2; k > 0; k /= 2 ) {
-        int num = n - k + 1;
-        pthread_t *th = new pthread_t[n - k + 1];
-        int l = 0;
-        for( i = k; i < n; i++ ) {
-            pthread_create(&( th[l] ),NULL,thread,(void*)&l);
-            l++;
+    //print(arr,n);
+    for( int k = n / 2; k > 1; k /= 2 ) {
+
+        pthread_t *th = new pthread_t[k];
+        for( int i = 0; i < k; i++ ) {
+            //arr2[j] = i + j * k;
+
+            int l = n / k;
+            int *arr2 = new int[l];
+            B *b =  new B;
+            b->arr = arr;
+            b->k = k;
+            b->i = i;
+            b->n = n;
+            for( int j = 0; j < n / k; j++ ) {
+
+                //std::cout << arr2[j] << " ";
+            }
+            pthread_create(&th[i],NULL,thread,(void*)b);
+            //std::cout << "" << '\n';
         }
-        for ( int p = 0; p < l; p++ ) {
-            pthread_join(th[p],NULL);
+        for( int i = 0; i < num; i++ ) {
+            pthread_join(th[i],NULL);
         }
-        std::cout << "/* message */" << '\n';
-        /*pthread_t *th = new pthread_t[n - k + 1];
-           for( i = k; i < n; i++ )
-           {
-            swap_struct *args = new swap_struct;
-            args->arr = arr;
-            args->n = n;
-            args->i = &i;
-            args->j = &j;
-            args->k = &k;
-            int e = pthread_create(&( th[i - k] ),NULL,thread,(void*)args);
-            std::cout << i << '\n';
-           }
-           for( int l = 0; l < n - k; l++ ) {
-            std::cout << pthread_join(th[i],NULL) << '\n';
-           }
-           delete th;*/
+        delete[] th;
     }
+    merge_sort(arr,n);
 }
 
 void *some_func(void* args){
@@ -121,10 +167,15 @@ public:
         this->arr = new int[n];
         memcpy(this->arr,arr,n * sizeof( int ) );
         this->n = n;
+
     }
 };
 
-
+void generate(int *arr,int n){
+    for( int i = 0; i < n; i++ ) {
+        arr[i] = rand() % 10000 - 500;
+    }
+}
 int main() {
 
     srand (time(NULL) );
@@ -132,10 +183,8 @@ int main() {
     clock_t t1, t2, t3, t4;
     int *arr = new int[n];
     int *arr2 = new int[n];
-    for( int i = 0; i < n; i++ ) {
-        arr[i] = rand() % 100000 - 500;
-    }
-    memcpy(arr2,arr,sizeof( int ) * n);
+    generate(arr,n);
+    generate(arr2,n);
     ShellsSort sh(arr,n);
     ShellsSort sh2(arr2,n);
 
@@ -145,18 +194,24 @@ int main() {
        std::cout << '\n';
      */
 
+    //print(arr,n);
+    //print(arr2,n);
     t1 = clock();
-    sh.sort(simple_sort);
+    shell(arr,n);
     t2 = clock();
-    std::cout << "\nTIME without multithreading : " << ( t2 - t1 ) / (double)CLOCKS_PER_SEC;
-    sh2.sort(func);
+    std::cout << "\nTIME without multithreading : " << ( t2 - t1 ) / (double)CLOCKS_PER_SEC << "\n";
     t3 = clock();
+    //sh2.sort(func);
+    func(arr2,n);
+    t4 = clock();
+    //print(arr,n);
+    //print(arr2,n);
     /*
        std::cout << '\n';
        for( int i = 0; i < n; i++ ) {
         std::cout << arr2[i] << ' ';
        }*/
 
-    /*std::cout << "\nTIME without multithreading : " << ( t3 - t2 ) / (double)CLOCKS_PER_SEC;*/
+    std::cout << "\nTIME with multithreading : " << ( t4 - t3 ) / (double)CLOCKS_PER_SEC << "\n";
     return 0;
 }
